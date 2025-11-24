185fcb739a4eae6914e52bf460dada288ca777b4
Hafiz <hafizmdph88172@gmail.com>
2025-11-24 22:36:01 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Recycle } from 'lucide-react';
-import { supabase } from '../lib/supabase';
+import { supabase } from "../lib/supabase.js";
+;
 
 export default function Register() {
   const navigate = useNavigate();
