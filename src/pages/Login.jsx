b956e49c3fbdff1786d1eead967a45a13b1cf47a
Hafiz <hafizmdph88172@gmail.com>
2025-11-24 22:42:57 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { Recycle } from 'lucide-react';
 import { supabase } from "../lib/supabase.js";
-;
+
+
 
 export default function Login() {
   const navigate = useNavigate();
